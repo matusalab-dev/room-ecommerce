814f0efb6220fd89224eb421e28f2b2ba706ab68
matusala <mtslabrh97@gmail.com>
2024-05-22 12:17:31 +0300

    chnages on main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-// import { StateProvider } from "./contexts/StateContext";
 import App from "./App";
+
 import { StateProvider } from "./contexts/StateContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
